45fca82f1ae386acb832170c17654e2aa10d4a61
misanchz98 <misanchz98@gmail.com>
2025-09-07 13:14:18 +0200

    fix(main): minor fixes

--- 03_modeling/utils/cross_validation.py
+++ 03_modeling/utils/cross_validation.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import itertools as itt
 import numbers
+from collections.abc import Iterable
 from abc import ABC, abstractmethod
 
 class BaseTimeSeriesCrossValidator:
